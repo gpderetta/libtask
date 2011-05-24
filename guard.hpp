04a0fca7d4b8fbe371b58f7c84dd217637047ad3
gpd <gpderetta@gmail.com>
2011-05-24 23:23:35 +0100

    simple scopeguard implementation

---
+++ guard.hpp
@@ -0,0 +1,19 @@
+#ifndef GPD_GUARD_HPP
+#define GPD_GUARD_HPP
+namespace gpd {
+namespace detail {
+template<class F>
+struct guard_t {
+    guard_t (F f) : dismissed(false), f(f) {}
+    bool dismissed;
+    F f;
+    void dismiss() { dismissed = true; }
+    ~guard() { if(!dismissed) f(); }
+}
+};
+}
+template<class F>
+guard_t<F> guard(F f) { return guard_t<F>(f); }
+
+}
+#endif
