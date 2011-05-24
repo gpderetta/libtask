f9f6c0b3f05fb1e34f5a9e2a6831ab1df46cba9e
gpd <gpderetta@gmail.com>
2011-05-24 23:23:56 +0100

    bitcast implementation

---
+++ bitcast.hpp
@@ -0,0 +1,18 @@
+#ifndef GPD_BITCAST_HPP
+#define GPD_BITCAST_HPP
+#include <type_traits>
+#include <cstring>
+namespace gpd {
+
+template<class A, class B>
+A bitcast(B b) {
+    static_assert(sizeof(A) == sizeof(b), "size mismatch");
+    static_assert(std::is_pod<A>::value, "not a POD");
+    static_assert(std::is_pod<B>::value, "not a POD");
+    A ret;
+    std::memcpy(&ret, &b, sizeof(b));
+    return ret;
+}
+
+}
+#endif
