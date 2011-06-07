a0784fe1da26f597cd63b501a3bd4a55a8383114
gpd <gpderetta@gmail.com>
2011-06-07 23:38:53 +0100

    some refactoring and cleanup

--- switch_base.hpp
+++ switch_base.hpp
@@ -2,11 +2,15 @@
 #define GPD_SWITCH_BASE_HPP
 #include <cassert>
 #include <stdint.h>
+#include <stddef.h>
 
 namespace gpd {
 typedef void* parm_t;
 
-struct cont { void * sp; explicit operator bool() const { return sp;}  };
+struct cont { 
+    void * sp; 
+    explicit operator bool() const { return sp;}
+};
 
 void * stack_bottom(void * vp, size_t size) {
     char * p = (char*)vp;
@@ -20,90 +24,75 @@ struct switch_pair {
     parm_t parm;
 };
 
-#define SAVE_ALL 1
+typedef switch_pair trampoline_t(parm_t parm, cont calling_continuation); 
 
+
+#define GPD_SAVE_REGISTERS                      \
+    "pushq %rbx            \n\t"                \
+    "pushq %r12            \n\t"                \
+    "pushq %r13            \n\t"                \
+    "pushq %r14            \n\t"                \
+    "pushq %r15            \n\t"                \
+/**/
+#define GPD_RESTORE_REGISTERS                   \
+    "popq %r15             \n\t"                \
+    "popq %r14             \n\t"                \
+    "popq %r13             \n\t"                \
+    "popq %r12             \n\t"                \
+    "popq %rbx             \n\t"                \
+/**/
 
 extern "C"
 switch_pair /*rax, rdx*/
 stack_switch_impl(cont sp /*rdi*/, parm_t parm /*rsi*/);
 asm volatile (                            
-".text                         \n\t"                              
-".weak stack_switch_impl            \n\t"                   
-".type stack_switch_impl, @function \n\t"            
-".align 16                     \n\t"                           
-"stack_switch_impl:                 \n\t"         
-        "pushq %rbp            \n\t"
-#if SAVE_ALL
-        "pushq %rbx            \n\t"
-        "pushq %r12            \n\t"
-        "pushq %r13            \n\t"
-        "pushq %r14            \n\t"
-        "pushq %r15            \n\t"
-#endif
-        "movq %rsi, %rdx       \n\t"  // parm-> switch_pair::parm
-        "movq %rsp, %rax       \n\t"  // rsp -> switch_pair::sp
-        "movq %rdi, %rsp       \n\t"  // sp  -> rsp
-#if SAVE_ALL
-        "popq %r15             \n\t"
-        "popq %r14             \n\t"
-        "popq %r13             \n\t"
-        "popq %r12             \n\t"
-        "popq %rbx             \n\t"
-#endif
-        "popq %rbp             \n\t"
-        "popq %rdi             \n\t"
-        "jmp  *%rdi            \n\t"  // jump to ret address
+    ".text                         \n\t"                              
+    ".weak stack_switch_impl       \n\t"                   
+    ".type stack_switch_impl, @function \n\t"            
+    ".align 16                     \n\t"                           
+    "stack_switch_impl:            \n\t"         
+    "pushq %rbp            \n\t"
+    GPD_SAVE_REGISTERS
+    "movq %rsi, %rdx       \n\t"  // parm-> switch_pair::parm
+    "movq %rsp, %rax       \n\t"  // rsp -> switch_pair::sp
+    "movq %rdi, %rsp       \n\t"  // sp  -> rsp
+    GPD_RESTORE_REGISTERS
+    "popq %rbp             \n\t"
+    "popq %rdi             \n\t"
+    "jmp *%rdi             \n\t"  // jump to ret address
     );
-
-switch_pair /*rax, rdx*/
-inline stack_switch(cont sp /*rdi*/, parm_t parm /*rsi*/) {
-    return stack_switch_impl(sp, parm);
-}
-
-typedef switch_pair trampoline_t(parm_t parm, cont calling_continuation);
 
 extern "C"
 switch_pair /*rax, rdx*/
 execute_into_impl(parm_t parm /*rdi*/, cont sp /*rsi*/, trampoline_t * ex /*rdx*/);
 asm volatile (                            
-".text                         \n\t"                              
-".weak execute_into_impl            \n\t"                   
-".type execute_into_impl, @function \n\t"            
-".align 16                     \n\t"                           
-"execute_into_impl:                 \n\t"                
-        "pushq %rbp            \n\t"
-#if SAVE_ALL
-        "pushq %rbx            \n\t"
-        "pushq %r12            \n\t"
-        "pushq %r13            \n\t"
-        "pushq %r14            \n\t"
-        "pushq %r15            \n\t"
-#endif
+    ".text                         \n\t"                              
+    ".weak execute_into_impl       \n\t"                   
+    ".type execute_into_impl, @function \n\t"            
+    ".align 16                     \n\t"                           
+    "execute_into_impl:            \n\t"                
+    "pushq %rbp            \n\t"
+    GPD_SAVE_REGISTERS
+    "movq %rsp, %rbx       \n\t"
+    "movq %rsi, %rsp       \n\t" 
+    "movq %rbx, %rsi       \n\t"
+    GPD_RESTORE_REGISTERS
+    "popq %rbp             \n\t"
+    "jmp *%rdx             \n\t"  //tail call (rdi is passed through)
+    );  
 
-#if 0
-        "movq %rsp, %rbx       \n\t"
-        "movq %rsi, %rsp       \n\t"
-        "movq %rbx, %rsi       \n\t"
-#else
-        "xchg %rsp, %rsi       \n\t"
-#endif
-
-#if SAVE_ALL
-        "popq %r15             \n\t"
-        "popq %r14             \n\t"
-        "popq %r13             \n\t"
-        "popq %r12             \n\t"
-        "popq %rbx             \n\t"
-#endif
-        "popq %rbp             \n\t"
-        "jmp  *%rdx            \n\t"  //tail call (rdi is passed through)
-    );
-
+inline switch_pair
+stack_switch(cont sp, parm_t parm) {
+    return stack_switch_impl(sp, parm);
+}
+   
 inline
-switch_pair /*rax, rdx*/
-execute_into(parm_t parm /*rdi*/, cont sp /*rsi*/, trampoline_t * ex /*rdx*/) {
+switch_pair 
+execute_into(parm_t parm, cont sp, trampoline_t * ex) {
     return execute_into_impl(parm, sp, ex);
 }
 
+
 }
 #endif
+
