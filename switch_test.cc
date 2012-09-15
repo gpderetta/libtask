3c65bdf4510750abdd2da31ee6370645a09d68db
Giovanni P. Deretta <gpderetta@gmail.com>
2012-09-15 02:00:33 +0100

    remove redundant lambda

--- switch_test.cc
+++ switch_test.cc
@@ -158,8 +158,7 @@ int main() {
     {
         std::vector<int> v = { 0,1,2,3,4,5,6,7,8,9,10 };
         auto f = [=](continuation<void(int)> c) { 
-            std::for_each(v.begin(), v.end(), [&](int i){ c(i); });
-            return c;
+            return std::for_each(v.begin(), v.end(), std::move(c));
         };
     
         for(auto i = callcc(f) ; i ; i++) {
