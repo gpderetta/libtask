f0680472a08f13404e1d692e5cf96abac65ec568
Giovanni P. Deretta <gpderetta@gmail.com>
2012-10-05 02:14:26 +0100

    remove dependency on boost

--- tuple.hpp
+++ tuple.hpp
@@ -1,25 +1,25 @@
 #ifndef TUPLE_HPP
 #define TUPLE_HPP
-#include <boost/mpl/identity.hpp>
 #include <tuple>
 
 #include "macros.hpp"
 namespace gpd {
 
+template<class T> struct identity { typedef T type; };
+
 template<int> struct placeholder {};
 namespace details {
-namespace mpl = boost::mpl;
 
 template<class T, class Disable=void>
-struct pack_arg : boost::mpl::identity<T&&> {};
+struct pack_arg : identity<T&&> {};
 
 template<int I>
-struct pack_arg<placeholder<I>> : boost::mpl::identity<placeholder<I>> {};
+struct pack_arg<placeholder<I>> : identity<placeholder<I>> {};
 
 template<int I>
-struct pack_arg<placeholder<I>&> : boost::mpl::identity<placeholder<I>> {};
+struct pack_arg<placeholder<I>&> : identity<placeholder<I>> {};
 template<int I>
-struct pack_arg<const placeholder<I>&> : boost::mpl::identity<placeholder<I>> {};
+struct pack_arg<const placeholder<I>&> : identity<placeholder<I>> {};
 }
 
 template<class ...Args>
