3596b9fedd6f2307da7656ce8ab7296b6d3bdc72
gpd <gpd@angband>
2015-01-15 22:48:06 +0000

    mpsc fixes

--- node.hpp
+++ node.hpp
@@ -1,11 +1,11 @@
 #ifndef GPD_NODE_HPP
 #define GPD_NODE_HPP
-#include "atomic.hpp"
+#include <atomic>
 namespace gpd {
 struct node
 {
     explicit node(node * next = 0) : m_next(next) {}
-    atomic<node*>  m_next;
+    std::atomic<node*>  m_next;
 };
 }
 #endif
