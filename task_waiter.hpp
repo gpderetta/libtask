e7eed07c9d9d70f7a3b61ebf318026f47769c23e
gpd <gpd@angband>
2015-02-26 23:28:48 +0000

    Simplify the task_wait wait_any_adl implementation to look similar to wait_adl

--- task_waiter.hpp
+++ task_waiter.hpp
@@ -6,79 +6,17 @@
 namespace gpd {
 using task_t = continuation<void()>;
 
-struct task_waiter_base : waiter {
-    event **children_begin, **children_end;
-    task_t next;
-
-    // whoever increments critical from 0 to 1 gets to call do_signal
-    // Starts at 1 to prevent signaling while we setup the waiters
-    std::atomic<std::size_t> critical = { 1 };
-
-    void wait(task_t& to) {
-        if (event::then(this, children_begin, children_end))
-            to = callcc
-                (std::move(to),
-                 [this](task_t c) {
-                    next = std::move(c);       
-                    
-                    // attempt to set critical to 0. On success, we are done
-                    if ( critical-- != 1)
-                        // signal was called at least once and critcal
-                        // incremented, but the starting non-zero value
-                        // prevented the signaler from calling
-                        // do_signal. We do it here instead.
-                        do_signal();
-                    return c;
-                });
-    }
-
-    void do_signal() {
-        std::size_t count = event::dismiss_then(this, children_begin, children_end);
-        // 'count' signalers got in before we could disengage their
-        // event. We need to wait them to signal that they are out of
-        // the critical section by each increasing the critical count by one.
-        //
-        // note that the signal side critical session is tiny.
-        
-        while(critical != count)
-              __builtin_ia32_pause();
-        
-        auto next = std::move(this->next);
-        next();   
-    }
-    
-    void signal(event_ptr p) override {
-        p.release();
-        if (critical++ == 0)
-            // critical was exactly 0, we are in charge
-            do_signal();
-    }
-
-    task_waiter_base(event** b, event** e) : children_begin(b), children_end(e) {}
-};
-
-template<int children_count>
-struct task_waiter : task_waiter_base {
-    event* children[children_count];
-    template<class... Waitable>
-    task_waiter(Waitable&... w)
-        : task_waiter_base(children, children + children_count)
-        , children{ get_event(w)...  }  {}
-};
-
 template<class Waitable>
 void wait_adl(task_t& to, Waitable& w) {
     if (get_event(w) == 0) return;
     struct task_latch : waiter {
         task_t next;
-        unsigned canary = 0xdeadbeef;
         void signal(event_ptr p) override {
             p.release();
             auto next = std::move(this->next);
-            canary = 0;
             next();
         }
-        ~task_latch() { assert(!canary);}
+        ~task_latch() { }
     } waiter;
 
     assert(to);
@@ -96,7 +34,50 @@ void wait_adl(task_t& to, Waitable& w) {
 
 template<class... Waitable>
 void wait_any_adl(task_t& to, Waitable&... w) {
-    task_waiter<sizeof...(w)>{ w... }.wait(to);
+    event* children[] = { get_event(w)... };
+
+    struct task_latch : waiter {
+        task_t next;
+        // whoever increments critical from 0 to 1 gets to call do_signal
+        // Starts at 1 to prevent signaling while we setup the waiters
+        std::atomic<std::size_t> critical = { 1 };
+
+        void signal(event_ptr p) override {
+            p.release();
+            if (critical++ == 0) {
+                auto next = std::move(this->next);
+                next();
+            }
+        }
+    } waiter;
+
+    if (!event::then(&waiter, std::begin(children), std::end(children)))
+        return;
+    
+    to = callcc
+        (std::move(to),
+         [&](task_t c) {
+            waiter.next = std::move(c);       
+                    
+            // attempt to set critical to 0. On success, we are done
+            if ( waiter.critical-- != 1) {
+                // signal was called at least once and critcal
+                // incremented, but the starting non-zero value
+                // prevented the signaler from calling
+                // do_signal. We do it here instead.
+                auto next = std::move(waiter.next);
+                next();
+            }
+            return c;
+        });
+    
+    std::size_t count =
+        event::dismiss_then(&waiter, std::begin(children), std::end(children));
+    
+    while(waiter.critical != count)
+        __builtin_ia32_pause();
+        
+
 }
 
 }
