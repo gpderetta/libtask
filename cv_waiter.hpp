3fb7e6a43658fae3bd5fe308949d62d5c9959fc9
gpd <gpd@angband>
2015-03-04 22:25:00 +0000

    cv_waiter: Remove custom wait_any implementation and add a task_waiter modelling the CountdownLatch concept instead.

--- cv_waiter.hpp
+++ cv_waiter.hpp
@@ -2,25 +2,36 @@
 #define GPD_CV_WAITER_HPP
 #include "event.hpp"
 #include <thread>
-
+#include <iostream>
 namespace gpd {
 struct cv_waiter : waiter {
-    event_ptr payload;
+    std::int32_t signal_counter = 0;
     std::mutex mux;
     std::condition_variable cvar;
 
-    void signal(event_ptr p) override {
-        { std::unique_lock<std::mutex>_ (mux); payload.swap(p); }
-        cvar.notify_one();
-    }
+    void reset() { signal_counter = 0; }
     
-    event_ptr wait()   {
-        std::unique_lock<std::mutex> lock (mux);
-        while(!payload) cvar.wait(lock);
-        return std::move(payload);
+    void signal(event_ptr p) override {
+        p.release();
+        bool signal = false;
+        {   std::unique_lock<std::mutex>_ (mux);
+
+            // thechnically <= would be safe. 
+            signal = (--signal_counter == 0);
+        }
+        if (signal)
+            cvar.notify_one();
     }
 
-    
+    void wait(std::uint32_t count = 1)   {
+        std::unique_lock<std::mutex> lock (mux);
+        signal_counter += count;
+        while(signal_counter > 0) {
+            cvar.wait(lock);
+        }
+    }
+
+
 };
 }
 #endif
