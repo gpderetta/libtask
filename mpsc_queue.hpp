b1f2334d2bb44c6e7a5600131e11fefb6124f290
gpd <gpd@angband>
2015-03-04 21:54:11 +0000

    Add _unlocked functions to mpsc queue

--- mpsc_queue.hpp
+++ mpsc_queue.hpp
@@ -22,17 +22,47 @@ struct mpsc_queue_base
         , m_tail(0)
     {}
 
-    void push(node* n) {
+    void push_unlocked(node* n, bool many = false) {
+        if (!many)
+            n->m_next.store(0, std::memory_order_relaxed);
         n->m_next.store(0, std::memory_order_relaxed);
+        node* prev = m_head.load(std::memory_order_relaxed);
+        XASSERT(prev);
+        m_head.store(n, std::memory_order_relaxed);
+        prev->m_next.store(n, std::memory_order_relaxed);
+    }
+
+    void push(node* n, bool many = false) {
+        if (!many)
+            n->m_next.store(0, std::memory_order_relaxed);
         node* prev = m_head.exchange(n);
         XASSERT(prev);
         prev->m_next.store(n, std::memory_order_release);
     }
 
-    node* pop()  {
+    node * pop_unlocked() {
+        auto tail = m_tail.m_next.load(std::memory_order_relaxed);
+        if (tail == 0)
+            return 0;
 
+        node* next = tail->m_next.load(std::memory_order_relaxed);
+
+        m_tail.m_next.store(next, std::memory_order_relaxed);
+
+        if (!next) {
+            // tail is the last elment, update head
+            XASSERT(tail == m_head.load(std::memory_order_relaxed));
+            m_head.store(&m_tail, std::memory_order_relaxed);  
+        }
+        
+        return tail;
+        
+    }
+   
+    // returns the next element
+    node * pop() {
         node* tail = m_tail.m_next.load(std::memory_order_acquire);
-        if (0 == tail)
+        if (tail == 0)
             return 0;
 
         node* next = tail->m_next.load(std::memory_order_acquire);
@@ -41,24 +71,40 @@ struct mpsc_queue_base
             m_tail.m_next.store(next, std::memory_order_relaxed);
             return tail;
         }
-
+        // caught up with the producers, need to exchange 
+        
         node* head = m_head.load(std::memory_order_acquire);
 
         if (tail != head)
+        {   
+            // producer preempted? we are stuck untill it finishes
             return 0;
+        }
+
         m_tail.m_next.store(0, std::memory_order_release);
 
         if (m_head.compare_exchange_strong(head,  &m_tail))  {
             return tail;
         }
 
+        // cas failed, a node was pushed
         next = tail->m_next.load(std::memory_order_acquire);
         if (next) {
             m_tail.m_next.store(next, std::memory_order_release);
             return tail;
         }
         return 0;
-    } 
+    }
+
+    node * peek() {
+        node* tail = m_tail.m_next.load(std::memory_order_acquire);
+        if (tail == 0)
+            return 0;
+
+        node* next = tail->m_next.load(std::memory_order_relaxed);
+
+        return next ? tail : 0;
+    }
 
     std::atomic<node*>  m_head;
     padding_t _;
@@ -86,8 +132,19 @@ struct mpsc_queue : mpsc_queue_base {
         mpsc_queue_base::push(n);
     }
 
+    void push_unlocked(node* n) {
+        mpsc_queue_base::push_unlocked(n);
+    }
+
     node *pop() {
         auto p = mpsc_queue_base::pop();
+        XASSERT(p != & m_tail);
+        return static_cast<node*>(p);
+    }
+
+    node *pop_unlocked() 
+    {
+        auto p = mpsc_queue_base::pop_unlocked();
         XASSERT(p != & m_tail);
         return static_cast<node*>(p);
     }
