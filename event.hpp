e5958f2c913259ba93b13e0ea9da24a0369e3a65
gpd <gpd@angband>
2015-03-04 22:19:40 +0000

    Implement generic wait_any/wait_all algos in term of a new CountdownLatch concept.
    Also rename then->wait.

--- event.hpp
+++ event.hpp
@@ -14,7 +14,7 @@ struct waiter {
 };
 
 struct delete_waiter_t : waiter {
-    virtual void signal(event_ptr) override {};
+    virtual void signal(event_ptr p) override { (void)p; };
     virtual ~delete_waiter_t() override {}
 };
 
@@ -24,7 +24,7 @@ extern delete_waiter_t delete_waiter;
 #define GPD_EVENT_IMPL_DEKKER_LIKE 2
 #define GPD_EVENT_IMPL_MIXED_ATOMICS 3
 
-#define GPD_EVENT_IMPL GPD_EVENT_IMPL_MIXED_ATOMICS
+#define GPD_EVENT_IMPL GPD_EVENT_IMPL_WAITFREE
 
 // Synchronize a producer and a consumer via a continuation.
 //
@@ -32,11 +32,10 @@ extern delete_waiter_t delete_waiter;
 // consumer. Signal may or may not synchronously invoke the consumer
 // registered continuation. 
 //
-// The consumer invokes 'then' to register a callback to listen for
-// the producer event. Once set with 'then' the callback cannot be unset.
+// The consumer invokes 'wait' (or try_then) to register a callback to
+// listen for the producer event. A registered callback can be unregistered .
 //
-// Alternatively the consumer can register a callback with 'wait'. In
-// this case the calback can be dismissed with reset.
+// The event can be in three states: empty, waited, signaled.
 //
 // Note: all RMW are seq_cst (because x86), but should be possible to
 // downgrade them to acq_rel
@@ -50,77 +49,94 @@ struct event
 {
     event(event&) = delete;
     void operator=(event&&) = delete;
-    event(bool shared = true) : state(shared ? state_t::shared : 0) {}
+    event(bool empty = true) : state(empty ? 0 : state_t::signaled ) {}
 
-    // Notify the consumer thread and relinquish ownership of the
-    // event.
+    // Put the event in the signaled state. If the event was in the
+    // waited state invoke the callback (and leave the event in the
+    // signaled state).
     void signal()  {
         auto val = get_state();
-        // not shared nor waited: can't happen
-        assert(val != 0);
+        assert(val != state_t::signaled);
         
-        val = state.exchange(0);
+        val = state.exchange(state_t::signaled);
 
-        if (val & state_t::waited) {
-            auto w = reinterpret_cast<waiter*>(val & state_t::waited);
+        if (val > state_t::signaled) {
+            auto w = reinterpret_cast<waiter*>(val);
             w->signal(
                 event_ptr (this));
         }
     }
 
-    // Register a consumer callback with the event relinquish
-    // ownership of the event. Pre: no dismissiable wait must be
-    // pending.
-    void then(waiter * w_) {
+    // Register a callback with the event. If the event is already
+    // signaled, invoke the callback (and leave the event in the
+    // signaled state), otherwise put the event to the waited state.
+    //
+    // Pre: state != waited
+    void wait(waiter * w_) {
+        if (!try_wait(w_))  
+            w_->signal(event_ptr(this));
+    }
+
+    // If the event is in the signaled state, return false, otherwise
+    // register a callback with the event and put the event in the
+    // waited state, then return true.
+    __attribute__((warn_unused_result))
+    bool try_wait(waiter * w_) {
         assert(w_);
         auto val = get_state();
-
-        // not already waited
-        assert(~val & state_t::waited);
-
         auto w = reinterpret_cast<std::uintptr_t>(w_);
-        // set the waiter and relinquish ownership
-        // returns true was still shared
-        if ( !(val != 0 && state.exchange(w)) )  {
-            // we need to call this ourselves
-            w_->signal(event_ptr(this));
-        }
+        return (val != state_t::signaled &&
+                state.compare_exchange_strong(val, w));
     }
-
+      
+    // [begin, end) is a range of pointers to events.  For every
+    // non-null pointer p in range, call p->try_wait(w) Return
+    // (signaled, waited), where signaled is number of times
+    // try_waited retuned false and waited is the number of times it
+    // retuned true.
     template<class Iter>
-    static  bool then(waiter * w, Iter begin, Iter end) {
-        bool waited = false;
-        for (auto i = begin; i != end; ++i)
-            if (*i) { waited = true; (**i).then(w); }
-        return waited;
+    static std::pair<std::size_t, std::size_t> wait_many(waiter * w, Iter begin, Iter end) {
+        std::size_t signaled = 0;
+        std::size_t waited  = 0;
+        for (auto  i = begin; i != end; ++i) {
+            if (*i) {
+                ((**i).try_wait(w) ? waited : signaled)++;
+            }
+        }
+        return std::make_pair(signaled, waited);
+
     }
     
-    // Dismiss a previous then(p) or successful try_then.  Returns
-    // true if the dismissal was successful. Otherwise the producer is
-    // in the process of inoking the callback; the consumer must wait
-    // for the signal and now owns the event.  Pre: last prepare_wait
-    // returned true and no retire_wait has been called since.
+    // If the event is in the waited or empty state, put it into the
+    // empty state and return true, otherwise return false and leave
+    // it in the signaled state.
     __attribute__((warn_unused_result))
-    bool dismiss_then(waiter* ) {
+    bool dismiss_wait(waiter* ) {
         auto val = get_state();
-        return val != 0 && state.compare_exchange_strong(val, state_t::shared);
+        return val == 0 ||
+            ( val != state_t::signaled &&
+              state.compare_exchange_strong(val, state_t::empty));
     }
 
+    // [begin, end) is a range of pointers to events.
+    // For every non-null pointer p in range, call p->try_wait(w)
+    // Return the number of successfull dismissals.
     template<class Iter>
-    static std::size_t dismiss_then(waiter * w, Iter begin, Iter end) {
+    static std::size_t dismiss_wait_many(waiter * w, Iter begin, Iter end) {
         std::size_t count = 0;
         for (auto  i = begin; i != end; ++i)
-            if (*i) count += ! (**i).dismiss_then(w);
+            if (*i) count += (**i).dismiss_wait(w);
         return count;
     }
 
     virtual ~event()  {}
 private:
     std::uintptr_t get_state() const { return state.load(std::memory_order_acquire); }
-    // msb is the shared bit
+    // msb is the signaled bit
     struct state_t {
-        static const constexpr std::uintptr_t shared = ~(std::uintptr_t(-1)>>1);
-        static const constexpr std::uintptr_t waited = ~shared;
+        static const constexpr std::uintptr_t empty = 0;
+        static const constexpr std::uintptr_t signaled = 1;
+        // waited is any value non empty non signaled
     };
     std::atomic<std::uintptr_t> state;
                              
@@ -238,7 +254,7 @@ private:
 /// waitfree one. It relies on a DCAS being able to synchronize with
 /// adjacent word-sized load and stores. The advantage is that no
 /// spinning is required as there is no signal side critical
-/// section.C++11 gives no guarantee, but depending on the underlying
+/// section. C++11 gives no guarantee, but depending on the underlying
 /// hardware it should work fine. Signal is lock-free, then and
 /// dismiss_then wait-free
 ///
@@ -351,30 +367,87 @@ event* get_event(T& x);
 
 /// ADL customization points for Waiters
 ///
-/// The default implementations expect the wait strategy to derive
-/// from 'waiter' and additonally expose a wait member
+/// The default implementations expect the wait strategy to match the
+/// CountdownLatch concept.
 ///
-/// @{
+///@{
+
+/// Note: this is not really an actual type but just a concept
+/// documentation.
+///
+/// A synchronization object modeling the CountdownLatch concept keeps
+/// track of the number of signaling operations since the last
+/// reset. Each inocation of signal increases the internal count,
+/// while each inocation of wait(n) consumes n previous invocations
+/// and decreases the count. Wait will not return untill count is at
+/// least n.
+struct CountdownLatch : waiter {
+    /// set count to 0. Not thread safe,
+    void reset();
+
+    /// call p.release(); increase count by 1. Wakeup waiter if //
+    /// target reached. Can be called concurrently with other calls to
+    /// signal and wait
+    void signal(event_ptr p);
+
+    /// Wait untill count == 'target', then decrease it by count. Can
+    /// be called concurrently with other calls to signal, but not
+    /// wait
+    void wait(std::uint32_t  target = 1);
+};
+
 
 template<class, class T = void>
 using void_t = T;
 
-template<class WaitStrategy, class Waitable>
-auto wait_adl(WaitStrategy& how, Waitable& what) ->
-    void_t<decltype(get_event(what))>{
-    get_event(what)->then(&how);
-    how.wait().release();
+template<class CountdownLatch, class Waitable>
+auto wait_adl(CountdownLatch& latch, Waitable& e) ->
+    void_t<decltype(get_event(e))>{
+    latch.reset();
+    get_event(e)->wait(&latch);
+    latch.wait();
 }
 
-template<class WaitStrategy, class... Waitable>
-void wait_all_adl(WaitStrategy& how, Waitable&... w) {
-    ([](...){})( (wait_adl(how, w), 0)... );
+template<class CountdownLatch, class... Waitable>
+void wait_all_adl(CountdownLatch& latch, Waitable&... e) {
+    event * events[] = {get_event(e)...};
+    latch.reset();
+    std::size_t waited =
+        event::wait_many(&latch, std::begin(events), std::end(events)).second;
+    if (waited)
+        latch.wait(waited);
 }
 
-template<class WaitStrategy, class... Waitable>
-void wait_any_adl(WaitStrategy& how, Waitable&... w) {
-    ([](...){})( (wait_adl(how, w), 0)... );
+template<class CountdownLatch, class... Waitable>
+void wait_any_adl(CountdownLatch& latch, Waitable&... e) {
+    event * events[] = {get_event(e)...};
+
+    latch.reset();
+    std::size_t signaled;
+    std::size_t waited;
+    std::tie(signaled, waited) =
+        event::wait_many(&latch, std::begin(events), std::end(events));
+
+    if (signaled == 0) 
+        latch.wait();
+        
+    const std::size_t dismissed =
+        event::dismiss_wait_many(&latch, std::begin(events), std::end(events));
+    
+    std::size_t pending = waited - dismissed;
+    assert( signaled != 0 || pending >= 1 );
+    if (signaled == 0) {
+        assert(pending >= 1);
+        pending -= 1;
+    }
+    
+    if (pending)
+        latch.wait(pending);
+
 }
+/// @} 
+
+
 /// @} 
 
 
