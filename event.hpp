92e84d194e43019f0db689585fcc24aa989f3e81
gpd <gpd@angband>
2015-03-13 23:54:06 +0000

    Remove broken MIXED_ATOMIC event implementation and reimplement it as a variant of the DEKKER_LIKE.

--- event.hpp
+++ event.hpp
@@ -39,17 +39,13 @@ extern noop_waiter_t noop_waiter;
 // consumer. Signal may or may not synchronously invoke the consumer
 // registered continuation. 
 //
-// The consumer invokes 'wait' (or try_then) to register a callback to
+// The consumer invokes 'wait' (or 'try_wait') to register a callback to
 // listen for the producer event. A registered callback can be unregistered .
 //
 // The event can be in three states: empty, waited, signaled.
-//
-// Note: all RMW are seq_cst (because x86), but should be possible to
-// downgrade them to acq_rel
-
 #if GPD_EVENT_IMPL == GPD_EVENT_IMPL_WAITFREE
-/// This is the straight-forward implementation. Signal and then use a
-/// stright exchange, while dismiss then uses a single strong CAS,
+/// This is the straight-forward implementation. 'Signal' uses a plain
+/// exchange, 'wait' and dismiss_wait' use a single strong CAS,
 /// Assuming exchange and CAS are waitfree, all operations are also
 /// waitfree.
 struct event 
@@ -137,33 +133,57 @@ private:
                              
 };
 
-#elif GPD_EVENT_IMPL == GPD_EVENT_IMPL_DEKKER_LIKE
+#elif (GPD_EVENT_IMPL == GPD_EVENT_IMPL_DEKKER_LIKE || \
+       GPD_EVENT_IMPL == GPD_EVENT_IMPL_MIXED_ATOMICS)
 
-/// This implementation uses a dekker-like mutual exclusion mechanism
-/// between signal and both then and dismiss_then. We trade an RMW
-/// with a store+fence+load and the possiblity of spinning, but we
-/// gain the ability to amortize the cost of the fence on multi event
-/// ops.
+/// The dekker-like implementation uses a pure store+load mutual
+/// exclusion mechanism between 'signal' and both 'wait' and
+/// 'dismiss_wait'. We trade an RMW with a store+fence+load and the
+/// possiblity of spinning, but we gain the ability to amortize the
+/// cost of the fence on multi event ops.
+///
+/// The mixed-atomix is an a small variation of the dekker-like
+/// implementation; 'signal' uses a DCAS loop to update the signaled field
+/// and atomically read the waited field, so a 'critical' state (and
+/// the corresponding critical section) is no longer necessary,
+/// removing the necessity of spinning on the 'wait' side; 'signal' is
+/// lock-free, 'wait' and 'dismiss_wait' are wait-free. This
+/// implementation relies on a double pointer sized DCAS being able to
+/// syncronize with pointer sized read ans stores; this is explicitly
+/// undefined behaviour in C++11, but it should work fine if the
+/// underlying hardware has native support for DCAS (or LL/SC).
 struct event 
 {
     event(event&) = delete;
     void operator=(event&&) = delete;
     event(bool shared = true) {
-        waited.store(0, std::memory_order_relaxed);
-        signaled.store(shared ? state_t::empty : state_t::signaled,
+        pair.waited.store(0, std::memory_order_relaxed);
+        pair.signaled.store(shared ? state_t::empty : state_t::signaled,
                        std::memory_order_relaxed);
     }
     
     void signal()  {
-        signaled.store(state_t::critical, std::memory_order_relaxed);
+#if    GPD_EVENT_IMPL == GPD_EVENT_IMPL_DEKKER_LIKE
+        pair.signaled.store(state_t::critical, std::memory_order_relaxed);
         std::atomic_thread_fence(std::memory_order_seq_cst);
-        auto w = waited.load(std::memory_order_acquire);
+        auto w = pair.waited.load(std::memory_order_acquire);
         if (!w)
-            signaled.store(state_t::signaled, std::memory_order_release);
+            pair.signaled.store(state_t::signaled, std::memory_order_release);
         else {
-            signaled.store(state_t::fired, std::memory_order_release);
+            pair.signaled.store(state_t::fired, std::memory_order_release);
             w->signal(event_ptr (this));
         }
+#else // GPD_EVENT_IMPL == GPD_EVENT_IMPL_MIXED_ATOMICS
+        assert(atomic_pair.is_lock_free());
+        auto w  = pair.waited.load(std::memory_order_relaxed);
+        atomic_pair_t old { w, state_t::empty };
+        while (! atomic_pair.compare_exchange_strong(
+                   old, 
+                   atomic_pair_t{ old.waited,
+                           old.waited ? state_t::fired : state_t:: signaled }))
+               ;         
+        if (old.waited) old.waited->signal(event_ptr (this));
+#endif        
     }
 
     void wait(waiter * w) {
@@ -175,7 +195,7 @@ struct event
         assert(w);
         bool waited = false;
         if (load_state() == state_t::empty) {
-            this->waited.store(w, std::memory_order_release);
+            pair.waited.store(w, std::memory_order_release);
             std::atomic_thread_fence(std::memory_order_seq_cst);
             waited = load_state() != state_t::signaled;
         }
@@ -185,7 +205,7 @@ struct event
 
     __attribute__((warn_unused_result))
     bool dismiss_wait(waiter*) {
-        waited.store(0, std::memory_order_release);
+        pair.waited.store(0, std::memory_order_release);
         std::atomic_thread_fence(std::memory_order_seq_cst);
         return was_waited && load_state() != state_t::fired;
     }
@@ -195,7 +215,7 @@ struct event
         for (auto i = begin; i != end; ++i) 
             if (auto e = get_event(*i)) {
                 e->was_waited = e->load_state() == state_t::empty;
-                e->waited.store(w, std::memory_order_release);
+                e->pair.waited.store(w, std::memory_order_release);
             }
         std::atomic_thread_fence(std::memory_order_seq_cst);
 
@@ -216,7 +236,7 @@ struct event
     static std::size_t dismiss_wait_many(waiter *, Iter begin, Iter end) {
         for (auto  i = begin; i != end; ++i)
             if (auto e = get_event(*i)) {
-                e->waited.store(0, std::memory_order_release);;
+                e->pair.waited.store(0, std::memory_order_release);;
             }
         std::atomic_thread_fence(std::memory_order_seq_cst);
         std::size_t count = 0;
@@ -232,127 +252,36 @@ private:
     enum class state_t { empty, critical, signaled, fired };
 
     state_t load_state() const {
-        auto s = signaled.load(std::memory_order_acquire);
+        auto s = pair.signaled.load(std::memory_order_acquire);
+#if    GPD_EVENT_IMPL == GPD_EVENT_IMPL_DEKKER_LIKE
         while (s == state_t::critical) {
-            s = signaled.load(std::memory_order_acquire);
+            s = pair.signaled.load(std::memory_order_acquire);
             __builtin_ia32_pause();
         }
+#endif
+        assert(s != state_t::critical);
         return s;
     }
+  
 
-    std::atomic<waiter*> waited;
-    std::atomic<state_t> signaled;
-    bool was_waited;
-                             
-} ;
-
-#elif GPD_EVENT_IMPL == GPD_EVENT_IMPL_MIXED_ATOMICS
-
-/// This is an hibrid between the dekker like implementation and the
-/// waitfree one. It relies on a DCAS being able to synchronize with
-/// adjacent word-sized load and stores. The advantage is that no
-/// spinning is required as there is no signal side critical
-/// section. C++11 gives no guarantee, but depending on the underlying
-/// hardware it should work fine. Signal is lock-free, then and
-/// dismiss_then wait-free
-///
-struct event 
-{
-    event(event&) = delete;
-    void operator=(event&&) = delete;
-    event(bool shared = true)
-        : atomic_pair { pair_t{ 0, shared ? state_t::unset : state_t::signaled } }
-    {  }
-    
-    // Notify the consumer thread and relinquish ownership of the
-    // event.
-    void signal()  {
-        auto w  = pair.waited.load(std::memory_order_relaxed);
-        pair_t old { w, state_t::unset };
-        while (! atomic_pair.compare_exchange_strong(
-                   old, 
-                   pair_t{ old.waited,
-                           old.waited ? state_t::fired : state_t:: signaled }))
-               ;         
-        if (old.waited) old.waited->signal(event_ptr (this));
-    }   
-    
-    // Register a consumer callback with the event. Pre: no
-    // dismissiable wait must be pending.
-    void then(waiter * w) {
-        assert(w);
-        then_1(w);
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        then_2(w);
-    }
-
-    template<class Iter>
-    static  bool then(waiter * w, Iter begin, Iter end) {
-        bool waited = false;
-        for (auto i = begin; i != end; ++i)
-            if (*i) { waited = true; (**i).then_1(w); }
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        for (auto i = begin; i != end; ++i)
-            if (*i) { (**i).then_2(w); }
-        return waited;
-    }
-    
-    // Dismiss a previous then(p) or successful try_then.  Returns
-    // true if the dismissal was successful. Otherwise the producer is
-    // in the process of inoking the callback; the consumer must wait
-    // for the signal and now owns the event.  Pre: last prepare_wait
-    // returned true and no retire_wait has been called since.
-    __attribute__((warn_unused_result))
-    bool dismiss_then(waiter* w) {
-        dismiss_then_1(w);
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        return dismiss_then_2(w);
-    }
-
-    template<class Iter>
-    static std::size_t dismiss_then(waiter * w, Iter begin, Iter end) {
-        for (auto  i = begin; i != end; ++i)
-            if (*i) (**i).dismiss_then_1(w);
-        std::atomic_thread_fence(std::memory_order_seq_cst);
-        std::size_t count = 0;
-        for (auto  i = begin; i != end; ++i)
-            if (*i) count += !(**i).dismiss_then_2(w);
-        return count;
-    }
-
-    virtual ~event()  {}
-private:
-    void then_1(waiter * w) { pair.waited.store(w, std::memory_order_release); }
-    void then_2(waiter * w) {
-        if (pair.signaled.load(std::memory_order_acquire) == state_t::signaled) {
-            w->signal(event_ptr(this));
-            pair.signaled.store(state_t::fired, std::memory_order_relaxed);
-        }
-    }
-
-    void dismiss_then_1(waiter*) { pair.waited.store(0, std::memory_order_release); }
-    bool dismiss_then_2(waiter*) {
-        return pair.signaled.load(std::memory_order_acquire) != state_t::fired;
-    }
-
-    
-    enum class state_t { unset, signaled, fired  };
-
-    struct pair_t {
+    struct atomic_pair_t {
         waiter* waited; 
         state_t signaled;
     };
 
-    struct atomic_pair_t {
-        std::atomic<waiter*> waited = { 0 };
-        std::atomic<state_t> signaled = { state_t::unset };
+    struct pair_t {
+    std::atomic<waiter*> waited;
+    std::atomic<state_t> signaled;
     };
-
-     union alignas(16) {
-        std::atomic<pair_t> atomic_pair;
-        atomic_pair_t pair;
+    
+    
+    union alignas(sizeof(pair_t)) {
+        std::atomic<atomic_pair_t> atomic_pair;
+        pair_t                     pair;
     };
-};                         
+    bool was_waited;
+                             
+} ;
 
 #endif
 
