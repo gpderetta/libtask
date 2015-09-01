832935d930b5903a82eeec83502b8b6908d72283
gpd <gpd@angband>
2015-09-01 23:57:08 +0100

    Some tweaks and additional tests constexpr lambda

--- lambda.cpp
+++ lambda.cpp
@@ -9,12 +9,28 @@
 
 template<class I>
 struct wrap {
-  constexpr wrap() = default;
-  template<class... Args>
+
+    template<class... Args>
     constexpr auto operator()(Args&&... args) const  {
-    using F = std::result_of_t<I(Args&&...)>;
-    return F()(std::forward<Args>(args)...);
-  }
+        using F = std::result_of_t<I(Args&&...)>;
+        return F()(std::forward<Args>(args)...);
+    }
+
+    template<class R, class... Args>
+    using _fn = R(Args...);
+
+    template<class R, class... Args>
+    constexpr operator _fn<R, Args...>*() const { return &wrap::_call; }
+    
+    template<class R, class... Args>
+    constexpr operator _fn<R, Args...>&() const { return &wrap::_call; }
+    
+private:
+    template<class... Args>
+    static constexpr auto _call(Args... args)  {
+        using F = std::result_of_t<I(Args&&...)>;
+        return F()(std::forward<Args>(args)...);
+    }
 };
 
 template<class F>
@@ -25,9 +41,9 @@ constexpr auto make(F*) {
 template<class T>
   constexpr T* ptr(T) { return nullptr; }
 
-
-#define body(...) noexcept(noexcept(__VA_ARGS__))\
-	{ return (__VA_ARGS__); }} _result; return _result; }))
+//noexcept(noexcept(__VA_ARGS__))               
+#define body(...) \
+{ return (__VA_ARGS__); }} _result; return _result; }))
 #define head(A, B) make(true ? nullptr : ptr([] A \
 	{ struct { constexpr auto operator() B body
 
@@ -42,6 +58,25 @@ template<int> struct integer {};
 template<int i>
 constexpr void foo(integer<i>){}
 
+template<class R, class F, class... T>
+struct table_t {
+    using fn =  R(void*, F& f);
+    fn* table[sizeof...(T)];
+};
+
+template<class R, class... T, class F>
+constexpr table_t<R, F, T...> table { $(cast)($(ptr, f)(f(*static_cast<std::decay_t<decltype(cast)> >(ptr))))((T*)0) ... };
+
+template<class R, class... T, class F>
+constexpr R cast(void * p, int index, F f) {
+    using fn =  R(void*, F& f);
+    //f(static_cast<T*>(ptr))
+    // constexpr fn* table[] = {
+    //     $(cast)($(ptr, f)(f(*static_cast<std::decay_t<decltype(cast)> >(ptr))))((T*)0) ...
+    // };                          
+    return table<R, F, T...>[index](p, f);
+}
+
 constexpr int bar(int k, int j) { return k + j;}
 int main()
 {
@@ -53,7 +88,7 @@ int main()
     {
         constexpr int i  = $(x)(x + 1)(42);
         static_assert(i == 43, "");
-        constexpr integer<i> x;
+        constexpr integer<i> x {};
         foo(x);
     }
     {
@@ -66,6 +101,21 @@ int main()
         static_assert(j == 1073, "");
 
     }
+    {
+        constexpr int(* p)(int)   = $(x)(x + 10);
+        static_assert(p(10) == 20, "");
+
+    }
+    {
+        constexpr int(& p)(int)   = $(x)(x + 10);
+        static_assert(p(10) == 20, "");
+
+    }
+    {
+        constexpr int j = 0;
+        constexpr int x = cast<int, int, float, double>(const_cast<int*>(&j), 0, $(x)(0));
+    }
+
 }
 
 
