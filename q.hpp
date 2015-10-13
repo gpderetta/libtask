347e1cf88c37a1fefe81bc4b506ccc61e25d24d6
gpd <gpd@angband>
2015-10-13 23:13:00 +0100

    Large changes to improve compilation performance and error messages

--- q.hpp
+++ q.hpp
@@ -38,17 +38,8 @@ struct constexpr_str
 
 struct probe { template<class T> constexpr operator T() const { return {}; } };
 
-template<class M, class T>
-using rebind_field = typename M::template field<T>;
-
-template<class M, class T, class D>
-struct field;
-
-template<class>
-struct meta_of_;
-
-template<class M, class T, class D>
-struct meta_of_<field<M,T,D> > { using type = M; };
+template<class Field>
+struct meta_of_ { using type = typename Field::__symbol_type; };
 
 template<class T>
 using meta_of_t = typename meta_of_<T>::type;
@@ -59,26 +50,20 @@ constexpr auto meta = meta_of_t<T>{};
 template<class T>
 constexpr auto meta_of(T&&) { return meta<std::decay_t<T> >; }
 
-// template<class M, class T, class D>
-// constexpr auto meta_of() { return M{}; }
-
-
 template<class T>
 constexpr auto get(T&& x) -> decltype((meta<std::decay_t<T> >(std::forward<T>(x)))) {
     return meta<std::decay_t<T> >(std::forward<T>(x));
 }
 
-template<class M, class T, class D>
-struct field : D
+template<class T, class Meta>
+using rebind_field_t = typename Meta::template field<T>;
+
+template<class Field, class Meta = meta_of_t<std::decay_t<Field> > >
+auto capture(Field&& x, Meta meta = Meta{}) ->
+    rebind_field_t<std::decay_t<decltype(meta(std::forward<Field>(x)))>, Meta>
 {
-    template<class T2>
-    field(T2&& x) : D{ std::forward<T2>(x) } {}
-
-    friend rebind_field<M, std::decay_t<T> > capture(const field& x) {
-        return { get(x) };
-    }
-
-};
+    return { meta(std::forward<Field>(x)) };
+}
 
 template<class T>
 constexpr constexpr_str name() {
@@ -86,7 +71,9 @@ constexpr constexpr_str name() {
 }
 
 template<class T>
-using gen = T(*)();
+struct gen { using type = T; };
+
+template<class T> T xgen() { return *(T*)0; }
 
 template<class T> struct init : T {  constexpr init() : T(*this){}; };
 template<class M>
@@ -96,23 +83,28 @@ struct symbol : M::fun, M::memfn, M::member {
     using M::member::operator();
 
     template< class T>
-    using field = decltype(typename M::field{}(symbol{}, gen<T>{}));
+    using field = std::result_of_t<typename M::field(gen<T>)>;
+    
+    template<class T>
+    auto operator=(T&& x) const -> field<T>  { return {std::forward<T>(x)};  }
 
     template<class T>
-    auto operator=(T&& x) const { return field<T>{std::forward<T>(x)};  }
+    friend auto operator<(T&& x, symbol) {
+        return {std::forward<T>(x)};
+    }
 
-    constexpr constexpr_str name() const { return gpd::name<typename M::name>(); }
+    constexpr constexpr_str name() const { return M{}.name(); }
     
 };
 
 
 template<class T>
-using capture_t = decltype(capture(std::declval<T>()));
+using capture_t = decltype(gpd::capture(std::declval<T>()));
 
 template<class... F>
 struct named_tuple : capture_t<F> ... {
     named_tuple(F... t)
-        : capture_t<F>(capture(t))...
+        : capture_t<F>{capture(t)}...
     { }
     
     template<typename V>
@@ -127,18 +119,17 @@ struct named_tuple : capture_t<F> ... {
 
 };
 
+struct eval { template<class... T> eval(T&&...){}};
+
 template<class... M>
 std::ostream& operator<<(std::ostream& s, named_tuple<M...> const& t)
 {
     s << "{";
                     
-    t.visit([&s](auto... t)
+    t.visit([&s](auto h, auto... t)
             {
-                bool first = true;
-                bool _ [] = {
-                    (s << (std::exchange(first, false) ? "" : ", ")
-                     << meta_of(t).name() << ": " << get(t), 0)... };
-                (void)_;
+                s << meta_of(h).name() << ": " << get(h);
+                eval{s << ", " << meta_of(t).name() << ": " << get(t)...};
             });
     return s << "}";
 }
@@ -158,54 +149,45 @@ named_tuple<T...> tup(T... t)
     return {std::forward<T>(t)...};
 };
 
-template<class Fun, class MemFn, class Member, class Field, class Name>
-struct X {
-    using fun = init<Fun>;
-    using memfn = init<MemFn>;
-    using member = init<Member>;
-    using field = init<Field>;
-    using name = Name;
-};
 
-template<class Fun, class MemFn, class Member, class Field, class Name>
-auto make_symbol(Fun, MemFn, Member, Field, Name) {
-    using X2 = X<Fun, MemFn, Member, Field, Name > ;
-    return gpd::symbol< X2 > {};
-    static_assert(sizeof(::gpd::symbol< X2 >) == 1, "not small");
-}
 }
 
 #define $forward($s) std::forward<decltype($s)>($s)
-#define $($s)                                                           \
-    (false?                                                             \
-     ::gpd::make_symbol(                                                \
-        [](auto&&... args)                                              \
-        noexcept(noexcept($s($forward(args)...)))                       \
-        -> decltype($s($forward(args)...)) {                            \
-            return $s($forward(args)...);                               \
-        },                                                              \
-        [](auto&& h, auto&&... args)                                    \
-        noexcept(noexcept($forward(h).$s($forward(args)...)))           \
-        -> decltype($forward(h).$s($forward(args)...)) {                \
-            return $forward(h).$s($forward(args)...);                   \
-        },                                                              \
-        [](auto&& h)                                                    \
-        noexcept(noexcept(h.$s))                                        \
-        -> decltype(($forward(h).$s)) {                                 \
-            return ($forward(h).$s);                                    \
-        },                                                              \
-        [](auto meta, auto g) noexcept {                                \
-            using T = decltype(g());                                    \
-            using M = decltype(meta);                                   \
-            struct C { T $s; };                                         \
-            return *(::gpd::field<M, T, C>*){};                        \
-        },                                                              \
-        [] {                                                            \
-            struct { constexpr auto name() const { return #$s; } } x;   \
-            return x;                                                   \
-        }                                                               \
-        ): ::gpd::probe{})                                              \
-          /**/
+#define $($s)                                                       \
+    (false?                                                         \
+     [] {                                                           \
+        struct meta;                                                \
+        using self_t = ::gpd::symbol<meta>;                         \
+        auto fun_ = [](auto&&... args)                              \
+            noexcept(noexcept($s($forward(args)...)))               \
+            -> decltype($s($forward(args)...))                      \
+            { return $s($forward(args)...); };                      \
+        auto memfn_ = [](auto&& h, auto&&... args)                  \
+            noexcept(noexcept($forward(h).$s($forward(args)...)))   \
+            -> decltype($forward(h).$s($forward(args)...))          \
+            { return $forward(h).$s($forward(args)...); } ;         \
+        auto member_ = [](auto&& h)                                 \
+            noexcept(noexcept($forward(h).$s))                      \
+            -> decltype(($forward(h).$s))                           \
+            { return ($forward(h).$s); };                           \
+                                                                    \
+        auto field_ = [](auto f) noexcept {                         \
+            struct field {                                          \
+                using __symbol_type = self_t;            \
+                typename decltype(f)::type $s;                  \
+                /*decltype(f) $x;*/                                 \
+            };                                                      \
+            return gpd::xgen<field>();                              \
+        };                                                          \
+        struct meta {                                               \
+            constexpr auto name() const { return #$s; }             \
+            using fun = ::gpd::init<decltype(fun_)>;                \
+            using memfn = ::gpd::init<decltype(memfn_)>;            \
+            using member = ::gpd::init<decltype(member_)>;          \
+            using field = ::gpd::init<decltype(field_)>;            \
+        } ; return gpd::symbol<meta>{}; }():                        \
+     ::gpd::probe{})                                                \
+     /**/
 
 
 #else
--- tests/q_test.cpp
+++ tests/q_test.cpp
@@ -127,16 +127,16 @@ int main() {
         rx.foo = 12;
         assert(x == 12);
 
-        auto cx = capture(rx);
+        auto cx = gpd::capture(rx);
         cx.foo = 42;
         assert(cx.foo == 42 && x == 12);
 
-        auto cy = capture($(bar) = x);
+        auto cy = gpd::capture($(bar) = x);
         cy.bar = 69;
         assert(cy.bar == 69 && x == 12);
 
         auto xbar = gpd::meta<std::decay_t<decltype(cy) > >;
-        auto cz = capture(xbar = 10);
+        auto cz = gpd::capture(xbar = 10);
         assert(cz.bar == 10);
 
         assert($(bar)(cz) == 10);
